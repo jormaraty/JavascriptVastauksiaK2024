f2e463c38a2cd89d9b2d7dffa34a080b943cac4b
jormaraty <jorma.raty@metropolia.fi>
2024-04-16 21:31:42 +0300

    eka versio

---
+++ mod-03/t5/5.js
@@ -0,0 +1,141 @@
+'use strict';
+
+const picArray = [
+  {
+    title: 'Title 1',
+    caption: 'Caption 1',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
+    image: {
+      large: 'img/pic1.jpg',
+      medium: 'thumbnails/pic1.jpg',
+    },
+  },
+  {
+    title: 'Title 2',
+    caption: 'Caption 2',
+    description:
+      'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
+    image: {
+      large: 'img/pic2.jpg',
+      medium: 'thumbnails/pic2.jpg',
+    },
+  },
+  {
+    title: 'Title 3',
+    caption: 'Caption 3',
+    description:
+      'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
+    image: {
+      large: 'img/pic3.jpg',
+      medium: 'thumbnails/pic3.jpg',
+    },
+  },
+  {
+    title: 'Title 4',
+    caption: 'Caption 4',
+    description:
+      'Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+    image: {
+      large: 'img/pic4.jpg',
+      medium: 'thumbnails/pic4.jpg',
+    },
+  },
+  {
+    title: 'Title 5',
+    caption: 'Caption 5',
+    description:
+      'Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. ',
+    image: {
+      large: 'img/pic5.jpg',
+      medium: 'thumbnails/pic5.jpg',
+    },
+  },
+  {
+    title: 'Title 6',
+    caption: 'Caption 6',
+    description:
+      'Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh.',
+    image: {
+      large: 'img/pic6.jpg',
+      medium: 'thumbnails/pic6.jpg',
+    },
+  },
+  {
+    title: 'Title 7',
+    caption: 'Caption 7',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
+    image: {
+      large: 'img/pic7.jpg',
+      medium: 'thumbnails/pic7.jpg',
+    },
+  },
+  {
+    title: 'Title 8',
+    caption: 'Caption 8',
+    description:
+      'Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. ',
+    image: {
+      large: 'img/pic8.jpg',
+      medium: 'thumbnails/pic8.jpg',
+    },
+  },
+  {
+    title: 'Title 9',
+    caption: 'Caption 9',
+    description:
+      'Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. ',
+    image: {
+      large: 'img/pic9.jpg',
+      medium: 'thumbnails/pic9.jpg',
+    },
+  },
+];
+
+// add your code here
+
+// etsitään web-sivulta kohta, johon js-koodin tulos lisätään (id='pictures')
+const targetElem = document.querySelector('#pictures');
+
+// käydään yllä olevan taulukon tiedot läpi for...of -toistolla
+for (let kuva of picArray)  {
+  // luodaan uusi article-elementti, jonka sisään yhden kuvan tiedot tulevat
+  let articleElem = document.createElement('article');
+
+  // ** määritetään articlen sisään tulevat elementit **
+
+  // määritellään articlen otsikko
+  let h2Elem = document.createElement('h2');
+  // lisätään otsikon teksti kuvan title-avaimen avulla
+  h2Elem.innerHTML = kuva.title;
+
+  // määritellään articlen figure tiedot
+  let figureElem = document.createElement('figure');
+  // figuren sisään tuleva img-elementti
+  let imgElem = document.createElement('img');
+  imgElem.src = kuva.image.medium;
+  imgElem.alt = kuva.title;
+  // figuren sisään tuleva figcaption-elementti
+  let figcaptionElem = document.createElement('figcaption');
+  figcaptionElem.innerHTML = kuva.caption;
+  // lisätään figure-elementin sisään img- ja figcaption-elementit
+  figureElem.appendChild(imgElem);
+  figureElem.appendChild(figcaptionElem);
+
+  // määritellään articlen description-tiedot
+  let pElem = document.createElement('p');
+  pElem.innerHTML = kuva.description;
+
+  // ** kootaan palapeli' eli lisätään articlen sisään sinne tulevat muut elementit **
+  articleElem.appendChild(h2Elem);
+  articleElem.appendChild(figureElem);
+  articleElem.appendChild(pElem);
+
+  // ** lisätään valmisarticle web-sivulle
+  targetElem.appendChild(articleElem);
+}
+
+
+
+
