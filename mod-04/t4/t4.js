9271a2875950ad91f1955b08a29d254eeb3c41d2
jormaraty <jorma.raty@metropolia.fi>
2024-04-18 22:57:05 +0300

    eka versio

---
+++ mod-04/t4/t4.js
@@ -0,0 +1,80 @@
+/*
+    Tämän 't4.js' koodi on käytännössä sama kuin 't3.js'.
+    Ainut ero on sarjan puuttuvan kuvan käsittely.
+    Tässä tiedostossa laitetaan puuttuvan kuvan paikalle
+    netistä haettava 'Not found'-kuva.
+    'Not found'-kuva voisi olla myös omassa img-kansiossa.
+ */
+
+'use strict';
+
+// etsitään html-sivulta div-elementti, jonka sisään js-koodin tulokset sijoitetaan
+const results = document.querySelector('#results');
+
+// etsitään html-sivulta syöttölomake (form)
+const tvForm = document.querySelector('#tv');
+
+// kun html-sivun lomakkeella painetaan nappulaa,
+// niin tämä lomakkeen tapahtumankäsittelijä suoritetaan
+tvForm.addEventListener('submit', async function (evt) {
+    // estetään lomakkeen oletustapahtuma
+    evt.preventDefault();
+    // luetaan lomakkeelta käyttäjän antama hakusana
+    const query = document.querySelector('input[name=q]').value;
+
+    // virhealtis netin kautta tehtävä datan kysely try-osion sisään
+    try {
+        // asynkroniset funktioiden kutsut
+        const response = await fetch(
+            `https://api.tvmaze.com/search/shows?q=${query}`,
+        );
+
+        // muutetaan edellä saatu Response-tyyppinen data json-dataksi
+        const jsonData = await response.json();
+        // debug: tulostetaan saatu json-data konsoliin
+        console.log(jsonData);
+
+
+        // ** datan tulostaminen web-sivulle alkaa **
+        // poistetaan mahdolliset entiset tulokset web-sivulta
+        results.innerHTML = '';
+
+        // käydään edellä saatu json-data läpi yksi sarja kerrallaan
+        for (const tvShow of jsonData) {
+            // luodaan articlen sisään tulevat html-tagit (elementit)
+            const h2 = document.createElement('h2');
+            h2.innerText = tvShow.show.name;
+            // kuvan käsittely
+            const img = document.createElement('img');
+            // huomaa: varaudutaan siihen, että show:n image-arvo puuttuu
+            /*
+                img.src = tvShow.show.image?.medium;
+                img.alt = tvShow.show.name;
+             */
+            // ns. kysymysmerkki-tekniikka (pikakoodausta, kaikki eivät tykkää)
+            // jos show.image löytyy -> valitaan '?' jälkeen oleva eka vaihtoehto,
+            // muuten valitaan toinen vaihtoehto. Vaihtoehdot erotellaan ':' merkillä
+            img.src = tvShow.show.image
+                ? tvShow.show.image.medium
+                : 'https://via.placeholder.com/210x295?text=No+Image';
+
+            // html-anakkurin (linkin) tekeminen
+            const a = document.createElement('a');
+            a.href = tvShow.show.url;
+            a.innerText = 'Go to TV Show';
+            // div-elementti, jonka sisälle tulee sarjan yhteenveto (summary)
+            const div = document.createElement('div');
+            div.innerHTML = tvShow.show.summary;
+            // luodaan article-elementti ja laitetaan edellä luodut elementit sen sisään
+            const article = document.createElement('article');
+            article.append(h2, img, a, div);
+
+            // lisätään valmis article-elementti html-sivulle
+            results.append(article);
+        }
+
+    } catch (error) {
+        // virhetilanteessa tullaan tänne
+        console.log(error.message);
+    }
+});
